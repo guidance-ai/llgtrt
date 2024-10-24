d3341f5ddeef2fc76f65cb1426b39b974c2260b6
Michal Moskal <michal@moskal.me>
2024-10-24 22:12:46 +0000

    fix error format

--- llgtrt/src/error.rs
+++ llgtrt/src/error.rs
@@ -1,7 +1,9 @@
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
+    Json,
 };
+use serde_json::json;
 
 #[derive(Debug)]
 pub struct AppError(anyhow::Error);
@@ -10,8 +12,9 @@ impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         (
             StatusCode::BAD_REQUEST,
-            format!("{}", self.0),
-            // "An error occurred while trying to fulfill your request.",
+            Json(json!({
+                "error": format!("{}", self.0),
+            })),
         )
             .into_response()
     }
