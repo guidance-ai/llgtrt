8b83c1a341795bf02febbdb6a3e6f243fdc730ac
Michal Moskal <michal@moskal.me>
2024-10-28 18:37:19 +0000

    cleaner cleanup

---
+++ docker/gh-cleanup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo Initial
+df -h /
+df /
+
+FOLDERS="
+    $AGENT_TOOLSDIRECTORY
+    /opt/hostedtoolcache
+    /opt/google/chrome
+    /opt/microsoft/msedge
+    /opt/microsoft/powershell
+    /opt/pipx
+    /usr/lib/mono
+    /usr/local/julia*
+    /usr/local/lib/android
+    /usr/local/lib/node_modules
+    /usr/local/share/chromium
+    /usr/local/share/powershell
+    /usr/share/dotnet
+    /usr/share/swift
+"
+
+for folder in $FOLDERS ; do
+    echo "Cleaning up $folder"
+    rm -rf $folder
+    df /
+done
+
+echo "Final"
+df -h /
